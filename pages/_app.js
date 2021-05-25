7b5ae2ea531cd0b324d962985438da4cca89dbea
Jakob Bourhil <jakob@genity.se>
2021-05-25 19:58:03 +0200

    Added Bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <h1>Hello World!</h1>
 }
 
 export default MyApp
